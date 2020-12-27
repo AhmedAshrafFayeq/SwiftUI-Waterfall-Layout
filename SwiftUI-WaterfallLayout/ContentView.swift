b2e048cb19d73603c24e7b55b661dc50863ca0a7
ahmedashraffayeq <ahmedashraffayeq@gmail.com>
2020-12-27 11:52:01 +0200

    first commit

---
+++ SwiftUI-WaterfallLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-WaterfallLayout
+//
+//  Created by Ahmed Fayek on 12/27/20.
+//  Copyright © 2020 Ahmed Fayek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
